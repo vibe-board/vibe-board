d517cd569d326221bb5e6d39326bd3d609e0e115
WqyJh <qiying-wang@qq.com>
2026-04-13 18:00:24 +0800

    feat: wire TabShell as top-level app wrapper, replacing GatewayProvider+GatewayGate

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
 // CSS is now imported by design scope components (LegacyDesignScope, NewDesignScope)
 import { ClickToComponent } from 'click-to-react-component';
-import { GatewayProvider } from '@/contexts/GatewayContext';
-import { GatewayGate } from '@/components/gateway/GatewayGate';
+import { TabShell } from '@/components/tabs/TabShell';
 import {
   QueryClient,
   QueryClientProvider,
@@ -86,11 +84,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
           showDialog
         >
           <ClickToComponent />
-          <GatewayProvider>
-            <GatewayGate>
-              <App />
-            </GatewayGate>
-          </GatewayProvider>
+          <TabShell />
           {/*<TanStackDevtools plugins={[FormDevtoolsPlugin()]} />*/}
           {/* <ReactQueryDevtools initialIsOpen={false} /> */}
         </Sentry.ErrorBoundary>
